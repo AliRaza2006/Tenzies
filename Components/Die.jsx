3cac6d36ee320df22f37555f8b614fde4a9df2c3
AliRaza2006 <maliraza2019@gmail.com>
2026-07-02 14:07:53 +0500

    converting all the numbers to dice

--- Components/Die.jsx
+++ Components/Die.jsx
@@ -2,7 +2,14 @@ export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
-    
+    const pattern={
+        1:[4],
+        2:[0,8],
+        3:[0,4,8],
+        4:[0,2,6,8],
+        5:[0,2,4,6,8],
+        6:[0,2,3,5,6,8]
+    }
     return (
         <button 
             style={styles}
@@ -10,6 +17,18 @@ export default function Die(props) {
             aria-pressed={props.isHeld}
             aria-label={`Die with value ${props.value}, 
             ${props.isHeld ? "held" : "not held"}`}
-        >{props.value}</button>
+        >
+            {(()=>{
+                const spans=[]
+                for(let i=0;i<9;i++)
+                {
+                    if(pattern[props.value].includes(i))
+                    spans.push(<span className="dot"></span>)
+                    else
+                    spans.push(<span></span>)   
+                }
+                return spans
+            })()}
+        </button>
     )
 }
